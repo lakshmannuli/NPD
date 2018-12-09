77d0474e44cf25ec8c8320ff0e372ef6cf4dcfa2
lakshmannuli <lakshmanjoyforever@gmail.com>
2018-12-09 19:22:39 +0530

    Added jobs

---
+++ Code/Web/NPD/NPD_UI/Controllers/JobController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NPD_UI.Controllers
+{
+    public class JobController : Controller
+    {
+        // GET: Job
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
