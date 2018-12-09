76c3332a52d41a57da511664588145486487ce5f
lakshmannuli <lakshmanjoyforever@gmail.com>
2018-12-09 19:13:58 +0530

    Added company

---
+++ Code/Web/NPD/NPD_UI/Controllers/ClientCompanyController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NPD_UI.Controllers
+{
+    public class ClientCompanyController : Controller
+    {
+        // GET: ClientCompany
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
