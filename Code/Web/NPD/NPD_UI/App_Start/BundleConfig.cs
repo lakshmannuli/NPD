6fcf2c66fd7eb585423694da8f756f520b66af29
lakshmannuli <lakshmanjoyforever@gmail.com>
2018-12-09 16:38:46 +0530

    Theme integrated

--- Code/Web/NPD/NPD_UI/App_Start/BundleConfig.cs
+++ Code/Web/NPD/NPD_UI/App_Start/BundleConfig.cs
@@ -8,24 +8,124 @@ namespace NPD_UI
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
+            // App Styles
+            bundles.Add(new StyleBundle("~/Content/appCss").Include(
+                "~/Content/app/css/app.css",
+                "~/Content/mvc-override.css"
+            ));
+
+            // Bootstrap Styles
+            bundles.Add(new StyleBundle("~/Content/bootstrapCss").Include(
+                "~/Content/app/css/bootstrap.css", new CssRewriteUrlTransform()
+            ));
+
+            bundles.Add(new ScriptBundle("~/bundles/Angle").Include(
+                // App init
+                "~/Scripts/app/app.init.js",
+                // Modules
+                "~/Scripts/app/modules/bootstrap-start.js",
+                //"~/Scripts/app/modules/calendar.js",
+                //"~/Scripts/app/modules/easypiechart.js",
+                "~/Scripts/app/modules/clear-storage.js",
+                "~/Scripts/app/modules/constants.js",
+                "~/Scripts/app/modules/flatdoc.js",
+                "~/Scripts/app/modules/trigger-resize.js",
+                "~/Scripts/app/modules/fullscreen.js",
+                //"~/Scripts/app/modules/gmap.js",
+                "~/Scripts/app/modules/load-css.js",
+                //"~/Scripts/app/modules/localize.js",
+                //"~/Scripts/app/modules/maps-vector.js",
+                //"~/Scripts/app/modules/navbar-search.js",
+                "~/Scripts/app/modules/notify.js",
+                "~/Scripts/app/modules/now.js",
+                "~/Scripts/app/modules/panel-tools.js",
+                "~/Scripts/app/modules/play-animation.js",
+                "~/Scripts/app/modules/porlets.js",
+                "~/Scripts/app/modules/sidebar.js",
+                "~/Scripts/app/modules/skycons.js",
+                "~/Scripts/app/modules/slimscroll.js",
+                "~/Scripts/app/modules/sparkline.js",
+                "~/Scripts/app/modules/table-checkall.js",
+                "~/Scripts/app/modules/toggle-state.js",
+                "~/Scripts/app/modules/utils.js",
+                //"~/Scripts/app/modules/chart.js",
+                //"~/Scripts/app/modules/morris.js",
+                //"~/Scripts/app/modules/rickshaw.js",
+                //"~/Scripts/app/modules/chartist.js",
+                //"~/Scripts/app/modules/tour.js",
+                "~/Scripts/app/modules/sweetalert.js",
+                //"~/Scripts/app/modules/color-picker.js",
+                //"~/Scripts/app/modules/imagecrop.js",
+                //"~/Scripts/app/modules/chart-knob.js",
+                //"~/Scripts/app/modules/chart-easypie.js",
+                "~/Scripts/app/modules/select2.js"
+            ));
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                      "~/Vendor/jquery/dist/jquery.js"
+          ));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Vendor/modernizr/modernizr.custom.js"
+            ));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Vendor/bootstrap/dist/js/bootstrap.js"
+            ));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            bundles.Add(new ScriptBundle("~/bundles/matchMedia").Include(
+                  "~/Vendor/matchMedia/matchMedia.js"
+          ));
+
+            bundles.Add(new ScriptBundle("~/bundles/storage").Include(
+            "~/Vendor/jQuery-Storage-API/jquery.storageapi.js"
+          ));
+            bundles.Add(new ScriptBundle("~/bundles/jqueryEasing").Include(
+           "~/Vendor/jquery.easing/js/jquery.easing.js"
+         ));
+
+            bundles.Add(new ScriptBundle("~/bundles/animo").Include(
+            "~/Vendor/animo.js/animo.js"
+          ));
+
+            bundles.Add(new ScriptBundle("~/bundles/slimscroll").Include(
+             "~/Vendor/slimscroll/jquery.slimscroll.min.js"
+           ));
+
+            bundles.Add(new ScriptBundle("~/bundles/screenfull").Include(
+           "~/Vendor/screenfull/dist/screenfull.js"
+         ));
+
+            bundles.Add(new ScriptBundle("~/bundles/demoRTL").Include(
+               "~/Scripts/demo/demo-rtl.js"
+           ));
+
+
+
+
+            bundles.Add(new StyleBundle("~/bundles/fontawesome").Include(
+          "~/Vendor/fontawesome/css/font-awesome.min.css", new CssRewriteUrlTransform()
+        ));
+
+            bundles.Add(new StyleBundle("~/bundles/simpleLineIcons").Include(
+            "~/Vendor/simple-line-icons/css/simple-line-icons.css", new CssRewriteUrlTransform()
+          ));
+            bundles.Add(new StyleBundle("~/bundles/animatecss").Include(
+                        "~/Vendor/animate.css/animate.min.css"
+            ));
+
+            bundles.Add(new StyleBundle("~/bundles/whirl").Include(
+            "~/Vendor/whirl/dist/whirl.css"
+          ));
+
+            bundles.Add(new StyleBundle("~/bundles/select2Css").Include(
+            "~/Vendor/select2/dist/css/select2.css",
+            "~/Vendor/select2-bootstrap-theme/dist/select2-bootstrap.css"
+          ));
+
+
+
         }
     }
 }
---
+++ Code/Web/NPD/NPD_UI/Scripts/demo/demo-upload.js
@@ -0,0 +1,28 @@
+// Upload Demo
+// ----------------------------------- 
+
+(function(window, document, $, undefined){
+
+    $(function () {
+        'use strict';
+
+        // Initialize the jQuery File Upload widget:
+        $('#fileupload').fileupload({
+            // Uncomment the following to send cross-domain cookies:
+            //xhrFields: {withCredentials: true},
+            // url: 'server/upload'
+        });
+
+        // Enable iframe cross-domain access via redirect option:
+        $('#fileupload').fileupload(
+            'option',
+            'redirect',
+            window.location.href.replace(
+                /\/[^\/]*$/,
+                '/cors/result.html?%s'
+            )
+        );
+
+    });
+
+})(window, document, window.jQuery);
